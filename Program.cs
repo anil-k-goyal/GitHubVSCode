2d011e612029e6ba339e30298c62206802d95aa9
anil.goyal <anilgoyal1984@gmail.com>
2020-04-12 23:24:44 +0530

    2nd change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace GitHubVSCode
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
